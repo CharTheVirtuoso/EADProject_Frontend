b4892eb4bc5e8d65515fb783c340072103b9a1e8
CharTheVirtuoso <chanukya.meshini.18@gmail.com>
2024-10-16 09:47:27 +0530

    feat/final adjustments

--- src/routes/VendorRoutes.js
+++ src/routes/VendorRoutes.js
@@ -5,35 +5,35 @@ import UserProfile from "../views/Vendor/UserProfile.js";
 
 var routes = [
   {
-    path: "/dashboard/:vendorId",
+    path: "/dashboard",
     name: "Vendor Dashboard",
     icon: "tim-icons icon-chart-pie-36",
     component: <Dashboard />,
     layout: "/vendor",
   },
   {
-    path: "/user-profile/:vendorId",
+    path: "/user-profile",
     name: "User Profile",
     icon: "tim-icons icon-single-02",
     component: <UserProfile />,
     layout: "/vendor",
   },
   {
-    path: "/products/:vendorId",
+    path: "/products",
     name: "Product Management",
     icon: "tim-icons icon-components",
     component: <ProductList />,
     layout: "/vendor",
   },
   // {
-  //   path: "/products/:vendorId",
+  //   path: "/products",
   //   name: "Inventory Management",
   //   icon: "tim-icons icon-basket-simple",
   //   component: <TableList />,
   //   layout: "/vendor",
   // },
   {
-    path: "/orders/:vendorId",
+    path: "/orders",
     name: "Order Management",
     icon: "tim-icons icon-delivery-fast",
     component: <OrderList />,
